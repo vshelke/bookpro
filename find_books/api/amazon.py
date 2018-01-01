71e7b16086d475bb110ac632a2a84c26180c2ef5
vshelke <vaibhavshelke017@gmail.com>
2018-01-02 00:27:56 +0530

    server

--- find_books/api/amazon.py
+++ find_books/api/amazon.py
@@ -1,29 +1,45 @@
 from hashlib import sha256
 from bs4 import BeautifulSoup
 import time, datetime, base64, hmac, requests, urllib, xmltodict, json
+import collections
 
 def amazon(s):
     access_key_id = 'AKIAJKBJSCFX6V2ODHAA'
     secret_key = "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"
     endpoint = "webservices.amazon.in"
     uri = "/onca/xml"
-    payload = {
-        'AWSAccessKeyId': access_key_id,
-        'AssociateTag': 'bookpro3301-21',
-        'Keywords': s,
-        'Operation': 'ItemSearch',
-        'ResponseGroup': 'Images,ItemAttributes,Offers',
-        'SearchIndex': 'Books',
-        'Service': 'AWSECommerceService',
-        #'Sort': 'relevancerank',
-        'Timestamp': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%SZ'),
-    }
+    payload = collections.OrderedDict(
+    [('AWSAccessKeyId', access_key_id), 
+     ('AssociateTag', 'bookpro3301-21'), 
+     ('Keywords', s),
+     ('Operation', 'ItemSearch'),
+     ('ResponseGroup', 'Images,ItemAttributes,Offers'),
+     ('SearchIndex', 'Books'),
+     ('Service', 'AWSECommerceService'),
+     #('Sort', 'relevancerank'),
+     ('Timestamp',datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%SZ')),
+    ])
+
+   # payload = {
+   #     'AWSAccessKeyId': access_key_id,
+   #     'AssociateTag': 'bookpro3301-21',
+   #     'Keywords': s,
+   #     'Operation': 'ItemSearch',
+   #     'ResponseGroup': 'Images,ItemAttributes,Offers',
+   #     'SearchIndex': 'Books',
+   #     'Service': 'AWSECommerceService',
+   #     #'Sort': 'relevancerank',
+   #     'Timestamp': '2017-12-27T02:20:19Z'#datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%SZ'),
+   # }
     canonical_querystring = urllib.parse.urlencode(payload).replace('+', '%20')
     string_to_sign = "GET\n" + endpoint + "\n" + uri + "\n" + canonical_querystring
+#    print (string_to_sign)
     dig = hmac.new( bytes(secret_key,'ascii'), msg=bytes(string_to_sign, 'ascii'), digestmod=sha256)
     sig = base64.b64encode(dig.digest())
+#    print (sig)
     payload['Signature'] = sig
     r = requests.get("http://" + endpoint + uri, params=payload)
+    #print (r.text)
     data = xmltodict.parse(r.text)['ItemSearchResponse']['Items']['Item']
     if len(data) > 0:
         pass
@@ -41,3 +57,6 @@ def amazon(s):
         'provider': 'https://images-na.ssl-images-amazon.com/images/G/01/SellerCentral/legal/amazon-logo_transparent.png'
     }
     return comp
+
+if __name__ == "__main__":
+	amazon("masala")
