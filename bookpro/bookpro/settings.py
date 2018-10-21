c02465615c185dd0c08f61f043417e579457f9d5
vshelke <vaibhavshelke017@gmail.com>
2018-10-21 13:53:15 +0530

    Update: static files enabled

--- bookpro/bookpro/settings.py
+++ bookpro/bookpro/settings.py
@@ -123,5 +123,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
